2afe42952b75280f1734985c40cfe0929cfe2191
unisty-h-innami <h_innami@unisty.co.jp>
2015-10-27 18:52:32 +0900

    う

--- src/test.java
+++ src/test.java
@@ -2,8 +2,12 @@ test2a
 ishidaaaaaaaaaasdadsa
 public static test aaaaa(){
 <<<<<<< HEAD
+<<<<<<< HEAD
     Logger.warn("これはテストです");あ　　　いう
 =======
     Logger.warn("これはテストです");あ　　　い
 >>>>>>> コミットコメント修正
+=======
+    Logger.warn("これはテストです");あ　　　いう
+>>>>>>> う
 }
