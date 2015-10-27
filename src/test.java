1fa8e99743d0fd56ed7f644ca6df6377fcea7e4b
unisty-h-innami <h_innami@unisty.co.jp>
2015-10-27 17:07:42 +0900

    EGitテスト

--- src/test.java
+++ src/test.java
@@ -1,7 +1,5 @@
 test2a
 ishidaaaaaaaaaasdadsa
 public static test aaaaa(){
-    Logger.info("test");
-+
-public static test aaaaa
-	----------------------------------------------------------------------------------------------
+    Logger.info("これはテストです");
+}
