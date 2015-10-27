5d1d8f7b626db7c7f2e7e2efcab2863ba1287f4e
unisty-r-ikumi <r_ikumi@unisty.co.jp>
2015-10-27 16:56:28 +0900

    wtawetea

--- src/test.java
+++ src/test.java
@@ -2,6 +2,6 @@ test2a
 ishidaaaaaaaaaasdadsa
 public static test aaaaa(){
     Logger.info("test");
-}
++
 public static test aaaaa
 	----------------------------------------------------------------------------------------------
