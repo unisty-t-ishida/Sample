d100dc99f422999438df6a284b9b7f64bfa6e7a8
unisty-r-ikumi <r_ikumi@unisty.co.jp>
2015-10-27 17:09:52 +0900

    コミットテスト

--- src/test.java
+++ src/test.java
@@ -2,8 +2,15 @@ test2a
 ishidaaaaaaaaaasdadsa
 public static test aaaaa(){
 <<<<<<< HEAD
+<<<<<<< HEAD
     Logger.warn("これはテストです");あ　　　いう
 =======
     Logger.warn("これはテストです");あ　　　い
 >>>>>>> コミットコメント修正
 }
+=======
+    Logger.error("test");
++
+public static test aaaaa
+	----------------------------------------------------------------------------------------------
+>>>>>>> 043fbd2... testtest
