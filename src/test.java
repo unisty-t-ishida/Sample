d6f14d6cef1a3f032323d6382de9c17aa21da03d
unisty-h-innami <h_innami@unisty.co.jp>
2015-11-04 15:54:12 +0900

    Revert "う"
    This reverts commit 2afe42952b75280f1734985c40cfe0929cfe2191.

--- src/test.java
+++ src/test.java
@@ -2,12 +2,8 @@ test2a
 ishidaaaaaaaaaasdadsa
 public static test aaaaa(){
 <<<<<<< HEAD
-<<<<<<< HEAD
     Logger.warn("これはテストです");あ　　　いう
 =======
     Logger.warn("これはテストです");あ　　　い
 >>>>>>> コミットコメント修正
-=======
-    Logger.warn("これはテストです");あ　　　いう
->>>>>>> う
 }
